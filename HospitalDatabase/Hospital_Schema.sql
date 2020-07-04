489cd062779de9578afcdbb93f07688fdcc5ef6f
Mahir Rahman <mrahman@amctechnology.com>
2020-07-04 15:58:33 -0400

    changes v2

--- HospitalDatabase/Hospital_Schema.sql
+++ HospitalDatabase/Hospital_Schema.sql
@@ -1,21 +1,11 @@
-/*
-THINGS TO POLISH: 
-We might Need to to add existing attributes from one table to another as like foreign key or smth like that so it can be accessed
-I think we need a department table
-
-**THINGS TO ADD: 
-VIEWS, STORED PROCEDURES, AND 20 QUERIES
- */
-
-CREATE TABLE employees
-(
-    emp_id NUMBER(6) NOT NULL,
+CREATE TABLE employees(
+    emp_id INT(6) NOT NULL,
     employee_type VARCHAR(25),
-    department_id NUMBER(2) NOT NULL,
-    first_name VARCHAR2(20),
-    last_name VARCHAR2(25) CONSTRAINT emp_last_name_nn  NOT NULL,
-    gender VARCHAR2(1),
-    email VARCHAR2(25) CONSTRAINT emp_email_uq UNIQUE(email)
+    department_id INT(2) NOT NULL,
+    first_name VARCHAR(20),
+    last_name VARCHAR(25) CONSTRAINT emp_last_name_nn  NOT NULL,
+    gender VARCHAR(1),
+    email VARCHAR(25) CONSTRAINT emp_email_uq UNIQUE(email)
 
     PRIMARY KEY(emp_id, department_id),
     FOREIGN KEY(department_id) REFERENCES dept(department_id)
@@ -31,11 +21,11 @@ INSERT INTO employees VALUES
 
 
 CREATE TABLE dept(
-    department_id NUMBER(2),
-    department_name VARCHAR2(30),
+    department_id INT(2),
+    department_name VARCHAR(30),
 
-    PRIMARY KEY(department_id, department_name)
-)
+    PRIMARY KEY(department_id)
+);
 
 INSERT INTO dept VALUES
     (01, 'Administration'),
@@ -45,10 +35,10 @@ INSERT INTO dept VALUES
     (05, 'Nephrology');
 
 CREATE TABLE physician(
-    emp_id NUMBER(6) NOT NULL,
-    first_name VARCHAR2(20),
-    last_name VARCHAR2(25) CONSTRAINT emp_last_name_nn  NOT NULL,
-    department_name VARCHAR2(30),
+    emp_id INT(6) NOT NULL,
+    first_name VARCHAR(20),
+    last_name VARCHAR(25) CONSTRAINT emp_last_name_nn  NOT NULL,
+    department_name VARCHAR(30),
 
     PRIMARY KEY(emp_id, last_name),
     FOREIGN KEY(emp_id) REFERENCES dept(emp_id),
@@ -66,9 +56,9 @@ INSERT INTO physician VALUES
 
 
 CREATE TABLE nurse(
-    first_name VARCHAR2(20),
-    last_name VARCHAR2(25) CONSTRAINT emp_last_name_nn  NOT NULL PRIMARY KEY,
-    department_name VARCHAR2(30),
+    first_name VARCHAR(20),
+    last_name VARCHAR(25) CONSTRAINT emp_last_name_nn  NOT NULL PRIMARY KEY,
+    department_name VARCHAR(30),
 
     PRIMARY KEY(last_name),
 	FOREIGN KEY(department_name) REFERENCES dept(department_name),
@@ -80,15 +70,15 @@ INSERT INTO nurse VALUES
     ('Jackie', 'Smith', 'Pediatrics');
 
 CREATE TABLE patients( 
-    patient_id NUMBER(6) NOT NULL,
+    patient_id INT(6) NOT NULL,
     patient_name VARCHAR(50) DEFAULT NULL,
     d_o_b DATE,
-    gender VARCHAR2(1),
+    gender VARCHAR(1),
     complaint VARCHAR(50),
-    room_no NUMBER(3),
-    emp_id NUMBER(6) NOT NULL,
-    med_id NUMBER(2) NOT NULL,
-    tests_id NUMBER(2),
+    room_no INT(3),
+    emp_id INT(6) NOT NULL,
+    med_id INT(2) NOT NULL,
+    tests_id INT(2),
 
 
     PRIMARY KEY(patient_id, patient_name),
@@ -99,31 +89,31 @@ CREATE TABLE patients(
 );
 
 INSERT INTO patients VALUES
-    (173293, 'Mahir', STR_TO_DATE('6-01-1990', '%d-%m-%Y'), 'M', 'High Blood Pressure', 201, 763901, 53),
-    (128312, 'Tracy', STR_TO_DATE('7-26-1970', '%d-%m-%Y'), 'F', 'Dizziness', 202, 763901, 22),
-    (672081, 'Stephanie', STR_TO_DATE('8-01-2006', '%d-%m-%Y'), 'F', 'Skin Rash', 205, 123456, 21),
-    (219903, 'Nathaniel', STR_TO_DATE('8-13-2000', '%d-%m-%Y'), 'M', 'Allergies', 206, 123456, 32);
+    (173293, 'Mahir', STR_TO_DATE('06-01-1990', '%d-%m-%Y'), 'M', 'High Blood Pressure', 201, 763901, 53),
+    (128312, 'Tracy', STR_TO_DATE('07-26-1970', '%d-%m-%Y'), 'F', 'Dizziness', 202, 763901, 22),
+    (672081, 'Stephanie', STR_TO_DATE('08-01-2006', '%d-%m-%Y'), 'F', 'Skin Rash', 205, 123456, 21),
+    (219903, 'Nathaniel', STR_TO_DATE('08-13-2000', '%d-%m-%Y'), 'M', 'Allergies', 206, 123456, 32);
 
 
 CREATE TABLE appointments(
-    appt_id NUMBER(5),
+    appt_id INT(5),
     dates DATE,
-    status_update VARCHAR2(11),
+    status_update VARCHAR(11),
 
     PRIMARY KEY(appt_id)
 );
 
 INSERT INTO appointments VALUES
-    (23612, STR_TO_DATE('6-01-2020', '%d-%m-%Y'), 'Completed'),
-    (26789, STR_TO_DATE('7-26-2020', '%d-%m-%Y'), 'Ongoing'),
-    (01296, STR_TO_DATE('8-01-2020', '%d-%m-%Y'), 'Delayed'),
-    (13530, STR_TO_DATE('8-13-2020', '%d-%m-%Y'), 'Unconfirmed');
+    (23612, STR_TO_DATE('06-01-2020', '%d-%m-%Y'), 'Completed'),
+    (26789, STR_TO_DATE('07-26-2020', '%d-%m-%Y'), 'Ongoing'),
+    (01296, STR_TO_DATE('08-01-2020', '%d-%m-%Y'), 'Delayed'),
+    (13530, STR_TO_DATE('08-13-2020', '%d-%m-%Y'), 'Unconfirmed');
 
 
 CREATE TABLE receptionist(
-    appt_id NUMBER(5) PRIMARY KEY,
-    last_name VARCHAR2(25) CONSTRAINT emp_last_name_nn  NOT NULL,
-    department_name VARCHAR2(30),
+    appt_id INT(5) PRIMARY KEY,
+    last_name VARCHAR(25) CONSTRAINT emp_last_name_nn  NOT NULL,
+    department_name VARCHAR(30),
 
 	FOREIGN KEY(appt_id) REFERENCES appointments(appt_id),
     FOREIGN KEY(department_name) REFERENCES dept(department_name),
@@ -138,9 +128,9 @@ INSERT INTO receptionist VALUES
 
 
 CREATE TABLE medication(
-    med_id NUMBER(2),
-    quantity NUMBER(3),
-    prescription VARCHAR2(50),
+    med_id INT(2),
+    quantity INT(3),
+    prescription VARCHAR(50),
 
     PRIMARY KEY(med_id)
 );
@@ -153,8 +143,8 @@ INSERT INTO medication VALUES
 
 
 CREATE TABLE medicalRecord(
-    record_id NUMBER(5),
-    pre_existing_cond VARCHAR2(50),
+    record_id INT(5),
+    pre_existing_cond VARCHAR(50),
     accessed_date DATE,
 
     PRIMARY KEY(record_id)
@@ -162,17 +152,17 @@ CREATE TABLE medicalRecord(
 
 
 INSERT INTO medicalRecord VALUES
-    (02345, 'Diabetes', STR_TO_DATE('5-03-2020', '%d-%m-%Y')),
-    (02567, 'ADHD', STR_TO_DATE('7-24-2020', '%d-%m-%Y')),
-    (02982, 'Depression', STR_TO_DATE('7-29-2020', '%d-%m-%Y')),
-    (02334, 'High Blood Pressure', STR_TO_DATE('8-02-2020', '%d-%m-%Y'));
+    (02345, 'Diabetes', STR_TO_DATE('05-03-2020', '%d-%m-%Y')),
+    (02567, 'ADHD', STR_TO_DATE('07-24-2020', '%d-%m-%Y')),
+    (02982, 'Depression', STR_TO_DATE('07-29-2020', '%d-%m-%Y')),
+    (02334, 'High Blood Pressure', STR_TO_DATE('08-02-2020', '%d-%m-%Y'));
 
 
 
 
 CREATE TABLE tests(
-    tests_id NUMBER(2),
-    test_name VARCHAR2(50),
+    tests_id INT(2),
+    test_name VARCHAR(50),
 
     PRIMARY KEY(tests_id)
 );
@@ -185,10 +175,10 @@ INSERT INTO tests VALUES
 
 
 CREATE TABLE room(
-    room_no NUMBER(3) NOT NULL,
-    department_name VARCHAR2(30),
-    num_beds NUMBER(1),
-    room_status VARCHAR2(10)
+    room_no INT(3) NOT NULL,
+    department_name VARCHAR(30),
+    num_beds INT(1),
+    room_status VARCHAR(10),
 
     PRIMARY KEY(room_no),
     FOREIGN KEY(department_name) REFERENCES dept(department_name)
@@ -205,10 +195,10 @@ INSERT INTO room VALUES
 
 
 CREATE TABLE inventory(
-    masks_quantity NUMBER(3),
-    gloves_quantity NUMBER(23)
+    masks_quantity INT(3),
+    gloves_quantity INT(23),
 
-    PRIMARY KEY(masks_quantity, gloves_quantity),
+    PRIMARY KEY(masks_quantity, gloves_quantity)
 );
 
 INSERT INTO inventory VALUES
