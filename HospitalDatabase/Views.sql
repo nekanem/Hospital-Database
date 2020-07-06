0e750269afb52659b51aaf5c1bf7e2862e02efc9
Nathaniel Ekanem <ekanemnv@vcu.edu>
2020-07-06 14:30:58 -0400

    Added Views

--- HospitalDatabase/Views.sql
+++ HospitalDatabase/Views.sql
@@ -1,5 +1,25 @@
-CREATE VIEW AS Nurse_View AS;
+CREATE VIEW receptionistNames 
+AS
+SELECT first_name ||' '|| last_name AS 'Full Name'
+FROM employees
+WHERE employee_type = 'Receptionist';
+select * from receptionistNames;
 
-WHERE;
-select * from Nurse_View;
+CREATE VIEW glovesAmount
+AS
+SELECT gloves_quantity
+FROM inventory;
+select * from glovesAmount;
 
+CREATE VIEW availableTests
+AS
+SELECT test_name
+FROM tests;
+select * from availableTests;
+
+CREATE VIEW availableMedication
+AS
+SELECT prescription
+FROM medication;
+WHERE quantity > 0;
+select * from availableMedication;
