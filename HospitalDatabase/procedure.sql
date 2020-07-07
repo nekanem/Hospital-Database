c910a35a6c63b0bf022875052b8b9ff0ebe20dc0
original_hiro <51250939+Mahiro99@users.noreply.github.com>
2020-07-06 22:54:58 -0400

    Update procedure.sql
    fixed it up

--- HospitalDatabase/procedure.sql
+++ HospitalDatabase/procedure.sql
@@ -2,9 +2,7 @@ USE hospital;
 
 delimiter //
 
-GO
 CREATE PROCEDURE updatePatient(IN id_patient INT(6), IN their_complaint VARCHAR(50))
-AS
 BEGIN
 UPDATE patients
 SET complaint = their_complaint
@@ -13,4 +11,4 @@ END //
 
 delimiter;
 
-CALL updatePatient(173293, 'Low Blood Pressure');
+CALL updatePatient(173293, 'High Blood Pressure');
